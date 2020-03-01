97838c228b6cb5393c8e783b9aff757505acf19f
yunsangjun <ysj188cm@gmail.com>
2020-03-01 17:13:08 +0900

    Add posts api controller, dto and test code

---
+++ src/main/java/com/demo/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.demo.springboot.service.posts;
+
+import com.demo.springboot.domain.posts.PostsRepository;
+import com.demo.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
