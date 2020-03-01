a02404322f0af59ae3404acf9387dc826f729545
yunsangjun <ysj188cm@gmail.com>
2020-03-02 00:23:51 +0900

    Add update func

--- src/test/java/com/demo/springboot/doamin/posts/PostsRepositoryTest.java
+++ src/test/java/com/demo/springboot/doamin/posts/PostsRepositoryTest.java
@@ -25,7 +25,7 @@ public class PostsRepositoryTest {
     }
 
     @Test
-    public void savePost_retrievePost() {
+    public void savePost() {
         String title = "test";
         String content = "This is a test.";
 
